7d192c0f21a1ed9a32890be02b6df3858775673f
Wania Khan <131209600+Wania-khan76@users.noreply.github.com>
2024-01-13 16:42:05 +0500

    Update WeatherAPI.h

--- Finalizing CEP CEW/WeatherAPI.h
+++ Finalizing CEP CEW/WeatherAPI.h
@@ -4,13 +4,16 @@
 #include <curl/curl.h>
 #include <cjson/cJSON.h>
 
+// Structure to hold retrieved data
 struct MemoryStruct {
     char *memory;
     size_t size;
 };
 
+// Callback function to write retrieved data to a memory buffer
 size_t write_callback(void *contents, size_t size, size_t nmemb, struct MemoryStruct *mem);
 
+// Function to fetch weather data from a specified URL and save it to a file
 int fetch_weather_data(const char *url, const char *output_filename);
 
 #endif  // WEATHER_API_H
