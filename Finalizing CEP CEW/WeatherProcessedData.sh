384306bed32528d128696389c6c6d7bd1f50fd6a
Wania Khan <131209600+Wania-khan76@users.noreply.github.com>
2024-01-13 17:30:15 +0500

    adding comments and changing updating files

--- Finalizing CEP CEW/WeatherProcessedData.sh
+++ Finalizing CEP CEW/WeatherProcessedData.sh
@@ -1,17 +1,21 @@
-#!/bin/bash
-
+# List of source files to compile and execute
 files=("WeatherAPI.c" "ProcessData.c")
 
+# Loop through each source file
 for file in "${files[@]}"; do
+    # Extract the filename without the extension
     filename="${file%.*}"
 
+    # Compile the source file using GCC with necessary libraries and include paths
     gcc -o "$filename" "$file" -lcjson -lcurl -I/ahzam/include/cjson -I/ahzam/include
 
+    # Check if the compilation was successful
     if [ $? -eq 0 ]; then
+        # Print a success message and execute the compiled program
         echo "Compilation successful for $file"
         "./$filename"
     else
+        # Print a failure message and skip execution if compilation failed
         echo "Compilation failed for $file. Skipping execution."
     fi
 done
-
