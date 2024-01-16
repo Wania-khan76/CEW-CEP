0475a94aae70769d05b8a14c7eb57263010bb92e
Ahzam Rehan <122669657+AhzamRB@users.noreply.github.com>
2024-01-16 20:54:07 +0500

    Updated WeatherAPI.h

--- Finalizing CEP CEW/WeatherAPI.h
+++ Finalizing CEP CEW/WeatherAPI.h
@@ -13,7 +13,4 @@ struct MemoryStruct {
 // Callback function to write retrieved data to a memory buffer
 size_t write_callback(void *contents, size_t size, size_t nmemb, struct MemoryStruct *mem);
 
-// Function to fetch weather data from a specified URL and save it to a file
-int fetch_weather_data(const char *url, const char *output_filename);
-
 #endif  // WEATHER_API_H
